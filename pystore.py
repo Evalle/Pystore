0c779c602c4c6c69f2af6e3ca4eea0e4bf1cd6ba
Evalle <shmarnev@gmail.com>
2014-04-09 11:52:00 +0600

    Update pystore.py
    add total counter

--- pystore.py
+++ pystore.py
@@ -1,19 +1,23 @@
 prices = {
-    "banana": 4, 
-    "apple": 2,
-    "orange": 1.5,
-    "pear":3
+    "banana" : 4,
+    "apple"  : 2,
+    "orange" : 1.5,
+    "pear"   : 3,
 }
-
 stock = {
-    "banana": 6,
-    "apple": 0,
-    "orange": 32,
-    "pear": 15
+    "banana" : 6,
+    "apple"  : 0,
+    "orange" : 32,
+    "pear"   : 15,
 }
 
-for key in prices: 
+for key in prices:
     print key
-    print "price: %s" %prices[key]
-    print "stock: %s" %stock[key]
-    print 
+    print "price: %s" % prices[key]
+    print "stock: %s" % stock[key]
+    print
+    
+total = 0 
+for key in prices:
+    total = total + prices[key]*stock[key]
+print "total cost: ", total

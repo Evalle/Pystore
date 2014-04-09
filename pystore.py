85c0573dcd64806c7bef3fc3961e38b043b6d943
Evalle <shmarnev@gmail.com>
2014-04-09 13:30:15 +0600

    Update pystore.py

--- pystore.py
+++ pystore.py
@@ -21,3 +21,28 @@ total = 0
 for key in prices:
     total = total + prices[key]*stock[key]
 print "total cost: ", total
+
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+    
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+    total = 0
+    for item in food:
+        if stock[item] > 0:
+            total += prices[item]
+            stock[item] -= 1
+    return total
